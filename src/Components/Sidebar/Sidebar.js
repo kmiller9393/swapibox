510487cf412d81c2779b40d829d1324725be9cf4
Kurt Miller <kmiller9393@gmail.com>
2018-08-20 16:37:29 -0600

    Add fetch call functionality to Sidebar component to render random episode information

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,22 +4,36 @@ export default class Sidebar extends Component {
   constructor() {
     super();
     this.state = {
+      title: '',
+      episode: null,
       quote: ''
-    }
+    };
   }
   componentDidMount = () => {
-    fetch(`https://swapi.co/api/films/${Math.floor(Math.random() * (7)) + 1}`)
-    .then(response => response.json())
-    .then(response => {
-      const quote = response.opening_crawl;
-      this.setState({ quote });
-      })
-    };
+    fetch(`https://swapi.co/api/films/${Math.floor(Math.random() * 7) + 1}`)
+      .then(response => response.json())
+      .then(response => {
+        const quote = response.opening_crawl;
+        const title = response.title;
+        const episode = response.episode_id;
+        this.setState({ title, episode, quote });
+      });
+  };
 
   render() {
     return (
-    <div className="sidebar">{this.state.quote}
-    </div>
+      <div className="sidebar">
+        <div className="fade" />
+        <section className="star-wars">
+          <div className="crawl">
+            <div className="title">
+              <p>Episode {this.state.episode}</p>
+              <h1>{this.state.title}</h1>
+            </div>
+            <p className="quote">{this.state.quote}</p>
+          </div>
+        </section>
+      </div>
     );
   }
 }
