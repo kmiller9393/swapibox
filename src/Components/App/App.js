b1552a2b6612f3425667fc3c03d11bf806603c55
benjaminpaige <benjamin.paige@gmail.com>
2018-08-21 23:12:48 -0600

    Set state with planets data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,29 +5,23 @@ import { Navbar } from '../Navbar/Navbar';
 import Sidebar from '../Sidebar/Sidebar';
 import People from '../../helper';
 import './App.css';
-import { peopleDataFetcher } from '../../api-helper';
+import { peopleDataFetcher, planetDataFetcher } from '../../api-helper';
 
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      people: [],
+      planets: []
+    };
   }
 
   componentDidMount = async () => {
-
     const people = await peopleDataFetcher()
-
-    // have all of the apis needed in an array and resolve all of them based on their endpoint (people vehicles planets)
-
-
-    // use the api for people and return an array of objects with all the needed data
-
-    // use the api for planets and return an array of objects with all the needed data
-    // use the api for vehicles and return an array of objects with all the needed data
-
-
-
+    this.setState({ people })
+    const planets = await planetDataFetcher()
+    this.setState({ planets })
   };
 
   render() {
