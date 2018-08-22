9806d5ae785eab624135d24b5ae091557b26d90f
benjaminpaige <benjamin.paige@gmail.com>
2018-08-21 21:49:35 -0600

    Set state to people array from helper function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,10 @@ import { CardContainer } from '../CardContainer/CardContainer';
 import Favorites from '../Favorites/Favorites';
 import { Navbar } from '../Navbar/Navbar';
 import Sidebar from '../Sidebar/Sidebar';
+import People from '../../helper';
 import './App.css';
+import { peopleDataFetcher } from '../../api-helper';
+
 
 export default class App extends Component {
   constructor() {
@@ -11,12 +14,20 @@ export default class App extends Component {
     this.state = {};
   }
 
-  componentDidMount = () => {
-    const peopleUrl = `https://swapi.co/api/people/`;
-    const peopleData = fetch(peopleUrl)
-      .then(response => response.json())
-      .then(data => data);
-    console.log(peopleData);
+  componentDidMount = async () => {
+
+    const people = await peopleDataFetcher()
+
+    // have all of the apis needed in an array and resolve all of them based on their endpoint (people vehicles planets)
+
+
+    // use the api for people and return an array of objects with all the needed data
+
+    // use the api for planets and return an array of objects with all the needed data
+    // use the api for vehicles and return an array of objects with all the needed data
+
+
+
   };
 
   render() {
