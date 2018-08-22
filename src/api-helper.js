a2ac6a48415aaeff846406719353422791cd87e4
benjaminpaige <benjamin.paige@gmail.com>
2018-08-21 23:12:21 -0600

    Add planetDataFetcher helper method

--- src/api-helper.js
+++ src/api-helper.js
@@ -1,51 +1,61 @@
-// import { peopleDataCleaner, getPeopleData } from './helper';
-
 const peopleUrl = `https://swapi.co/api/people/`;
+const planetsUrl = `https://swapi.co/api/planets/`;
+const vehiclesUrl = `https://swapi.co/api/vehicles/`;
 
 export const peopleDataFetcher = async () => {
-    // let array = []
     let response = await fetch(peopleUrl);
     let data = await response.json();
-    // i want to create an object for each person and their data and i want to push it into an array and return it, so i should use a method from the helper file to clean out the data
-
-    let bar = await peopleDataCleaner(data)
-
-    // let bar = await getPeopleData(data)
-
-    console.log(bar)
-    return data;
+    let results = await peopleDataCleaner(data)
+    return results;
   }
 
-  // export const dataFetcher = async (url) => {
-  //   console.log(url)
-  //   let response = await fetch(url);
-  //   let data = await response.json();
-  //   console.log(data)
-  //   return data
-  // }
-
   const peopleDataCleaner = (data) => {
-    // let array = []
-  let foo = data.results.map(async personData => {
+  let unresolvedPersonData = data.results.map(async personData => {
     let results = {};
     let homeworld = await fetchData(personData.homeworld);
     let species = await fetchData(personData.species[0]);
-
     results.name = personData.name;
     results.homeworld = homeworld.name
     results.species = species.name
     results.population = homeworld.population
-    // results.language = personData.language
   return results
   });
   
-  return Promise.all(foo)
+  return Promise.all(unresolvedPersonData)
   }
-  
-  
-  
+
+  export const planetDataFetcher = async () => {
+    let response = await fetch(planetsUrl);
+    let data = await response.json();
+    let results = await planetDataCleaner(data)
+    return results;
+  }
+
+  const getResidents = async (data) => {
+    const residents = data.residents.map(async (resident) => {
+      let residentName = await fetchData(resident);
+      return residentName.name
+    })
+    return Promise.all(residents)
+  }
+
+  const planetDataCleaner = (data) => {
+    let unresolvedPlanetData = data.results.map(async (data) => {
+      const residents = await getResidents(data)
+      let results = {};
+      results.name = data.name
+      results.terrain = data.terrain
+      results.population = data.population
+      results.climate = data.climate
+      results.residents = residents
+    return results
+  })
+  return Promise.all(unresolvedPlanetData)
+}
+
   const fetchData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     return data;
   }
+
