868ae0220fa4b9c5eeb22cd0810b6adc12078829
benjaminpaige <benjamin.paige@gmail.com>
2018-08-24 13:53:46 -0600

    Add test for class of button in Card component

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -20,6 +20,21 @@ describe('Card', () => {
     wrapper = shallow(<Card {...mockItem} favoriteItem={mockFavoriteItem} key={mockIndex} />)
   })
 
+  it('should start with a death-star class', () => {
+    expect(wrapper.find('button').hasClass('death-star')).toEqual(true);
+  })
+
+  it('if has property of favorite it should have class of death-star-active', () => {
+    mockItem = {
+      Class: "wheeled",
+      Model: "Digger Crawler",
+      Passengers: "30",
+      name: "Sand Crawler",
+      favorite: true}
+    wrapper = shallow(<Card {...mockItem} favoriteItem={mockFavoriteItem} key={mockIndex} />)
+    expect(wrapper.find('button').hasClass('death-star-active')).toEqual(true);
+  })
+
   it('Should invoke the favoriteItem method when btn clicked', () => {
     // const spy = jest.spyOn(wrapper.instance(), 'handleClick');
     // const mockEvent = {target: { id: 'SPECIAL EDUCATION'}};
