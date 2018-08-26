7c1ffaac6499bc0f06b313ee99cd085c712bdc59
Kurt Miller <kmiller9393@gmail.com>
2018-08-26 12:55:37 -0600

    Update App component styling with .scss

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { CardContainer } from '../CardContainer/CardContainer';
 import { Favorites } from '../Favorites/Favorites';
 import Navbar from '../Navbar/Navbar';
