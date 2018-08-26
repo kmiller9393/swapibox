f74a4d947dff09e47de9457cbb820ff09cd25816
benjaminpaige <benjamin.paige@gmail.com>
2018-08-25 19:38:03 -0600

    Refactor sidebar component

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -15,26 +15,28 @@ export default class Sidebar extends Component {
     const episodeNumber = Math.floor(Math.random() * 7) + 1;
     const result = await fetch(`https://swapi.co/api/films/${episodeNumber}`);
     const response = await result.json();
-    const quote = response.opening_crawl;
+    const { opening_crawl, episode_id, release_date } = response
+    const quote = opening_crawl;
     const title = response.title;
-    const episode = response.episode_id;
-    const releaseDate = response.release_date;
+    const episode = episode_id;
+    const releaseDate = release_date;
     this.setState({ title, episode, quote, releaseDate });
   };
 
   render() {
-    if (!this.state.quote) return <p className="sidebar" />;
+    const { quote, episode, title, releaseDate } = this.state
+    if (!quote) return <p className="sidebar" />;
     return (
       <div className="sidebar">
         <div className="fade" />
         <section className="star-wars">
           <div className="crawl">
             <div className="title">
-              <p>Episode {this.state.episode}</p>
-              <h1>{this.state.title}</h1>
+              <p>Episode {episode}</p>
+              <h1>{title}</h1>
             </div>
-            <p className="quote">{this.state.quote}</p>
-            <p>{this.state.releaseDate}</p>
+            <p className="quote">{quote}</p>
+            <p>{releaseDate}</p>
           </div>
         </section>
       </div>
